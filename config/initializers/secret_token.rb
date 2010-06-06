2ddf5c28bf388b944931c2034db0a0063c3d81c7
David Chelimsky <dchelimsky@gmail.com>
2010-06-06 15:57:02 -0400

    bootstrap new rails project with cukes and rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = 'f70be5efc6d8a58f64c48ae2a7935aff977c3f059ba364ad497c4188b822fbe43515f42819b09b40d13d2e4ddbd0545e84e906ad2471b779e93c92a0b10e5aba'
