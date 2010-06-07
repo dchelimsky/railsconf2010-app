37de2c0ccc72d2dca716fee23a3c6d4599903927
aslakhellesoy <aslak.hellesoy@gmail.com>
2010-06-07 01:04:55 -0400

    Implemented drag and drop. Capybara fucking rocks.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,18 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(function() {
+  $(".swimlane").sortable({
+    revert: true,
+    connectWith: '.swimlane',
+    
+    update: function(event, ui) {
+      var cardId = /card-(\d+)/.exec(ui.item.get(0).id)[1];
+      var swimlaneId = /swimlane-(\d+)/.exec(ui.item.parent().get(0).id)[1];
+      console.log(cardId + " -> " + swimlaneId);
+      $.ajax({
+        url: "/cards/" + cardId + "/moves",
+        data: {'card[swimlane_id]': swimlaneId},
+        type: 'POST'
+      })
+    }
+  });
+  $("ul, li").disableSelection();
+});
